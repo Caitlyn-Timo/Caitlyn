da80cf4ae3dfeed29c9d76a771b13f4e0f1beafb
Caitlyn-Timo <cocoroco@++1>
2019-04-16 10:32:24 +0800

    创新新项目caitlyn

---
+++ src/main/java/ccg/caitlyn/CaitlynApplication.java
@@ -0,0 +1,13 @@
+package ccg.caitlyn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaitlynApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaitlynApplication.class, args);
+    }
+
+}
